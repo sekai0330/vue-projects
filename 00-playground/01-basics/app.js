750a56d83fb10606de02368d659be529f34ce909
sekai0330 <asamieto965@gmail.com>
2021-05-16 14:00:37 +0200

    add conditional render methods

--- 00-playground/01-basics/app.js
+++ 00-playground/01-basics/app.js
@@ -1,11 +1,19 @@
 const app = Vue.createApp({
   //   template: "<h2>I am the template</h2>",
   data() {
-    return { title: "The Final Empire", author: "Brandon Sanderson", age: 45 };
+    return {
+      title: "The Final Empire",
+      author: "Brandon Sanderson",
+      age: 45,
+      showBooks: true,
+    };
   },
   methods: {
     changeTitle(title) {
       this.title = title;
+    },
+    toggleShowBooks() {
+      this.showBooks = !this.showBooks;
     },
   },
 });
