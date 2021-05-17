daea6bce2ee3bab79ad7f2342ca88c565bcad63b
sekai0330 <asamieto965@gmail.com>
2021-05-17 08:00:23 +0200

    add mouse events, lists, attr binding

--- 00-playground/01-basics/app.js
+++ 00-playground/01-basics/app.js
@@ -6,6 +6,29 @@ const app = Vue.createApp({
       author: "Brandon Sanderson",
       age: 45,
       showBooks: true,
+      x: 0,
+      y: 0,
+      books: [
+        {
+          title: "Name of the Wind",
+          author: "Patrick Rothfuss",
+          img: "https://github.com/iamshaunjp/Vue-3-Firebase/blob/lesson-13/assets/1.jpg?raw=true",
+          isFav: true,
+        },
+        {
+          title: "The Way of Kings",
+          author: "Brandon Sanderson",
+          img: "https://github.com/iamshaunjp/Vue-3-Firebase/blob/lesson-13/assets/2.jpg?raw=true",
+          isFav: false,
+        },
+        {
+          title: "The Final Empire",
+          author: "Brandon Sanderson",
+          img: "https://github.com/iamshaunjp/Vue-3-Firebase/blob/lesson-13/assets/3.jpg?raw=true",
+          isFav: true,
+        },
+      ],
+      url: "https://vuejs.org/",
     };
   },
   methods: {
@@ -14,6 +37,24 @@ const app = Vue.createApp({
     },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    handleEvent(e, data) {
+      console.log(e.type);
+      if (data) {
+        console.log(data);
+      }
+    },
+    handleMousemove(e) {
+      this.x = e.offsetX;
+      this.y = e.offsetY;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
