23d45629446551d9d47833faeb6bc7e729597ca3
sekai0330 <asamieto965@gmail.com>
2021-05-18 08:06:50 +0200

    add lifecycle hooks

---
+++ 00-playground/03-reaction-timer/src/components/Block.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="block" v-if="showBlock">click me</div>
+</template>
+
+<script>
+export default {
+  props: ["delay"],
+  data() {
+    return {
+      showBlock: false,
+    };
+  },
+  mounted() {
+    console.log("component mounted");
+    setTimeout(() => {
+      this.showBlock = true;
+    }, this.delay);
+  },
+  updated() {
+    console.log("component updated");
+  },
+  unmounted() {
+    console.log("component unmounted");
+  },
+};
+</script>
+
+<style>
+.block {
+  width: 400px;
+  border-radius: 20px;
+  background-color: #0faf87;
+  color: #fff;
+  text-align: center;
+  padding: 100px 0;
+  margin: 40px auto;
+}
+</style>
