1d228a35d8f512cff5fc55e15d32fc900eafa6af
sekai0330 <asamieto965@gmail.com>
2021-05-20 20:37:29 +0200

    add finished reaction timer

--- 00-playground/03-reaction-timer/src/App.vue
+++ 00-playground/03-reaction-timer/src/App.vue
@@ -1,27 +1,38 @@
 <template>
   <h1>Ninja Reaction Timer</h1>
   <button @click="start" :disabled="isPlaying">Play</button>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showResults" :score="score" />
 </template>
 
 <script>
 import Block from "./components/Block";
+import Results from "./components/Results";
 
 export default {
   name: "App",
   components: {
     Block,
+    Results,
   },
   data() {
     return {
       isPlaying: false,
       delay: null,
+      score: null,
+      showResults: false,
     };
   },
   methods: {
     start() {
+      this.showResults = false;
       this.delay = 2000 + Math.random() * 5000;
       this.isPlaying = true;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.showResults = true;
+      this.isPlaying = false;
     },
   },
 };
@@ -35,5 +46,20 @@ export default {
   text-align: center;
   color: #444;
   margin-top: 60px;
+}
+button {
+  background-color: #0faf87;
+  color: #fff;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
--- 00-playground/03-reaction-timer/src/components/Block.vue
+++ 00-playground/03-reaction-timer/src/components/Block.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="block" v-if="showBlock">click me</div>
+  <div class="block" v-if="showBlock" @click="stopTimer">click me</div>
 </template>
 
 <script>
@@ -8,12 +8,15 @@ export default {
   data() {
     return {
       showBlock: false,
+      timer: null,
+      reactionTime: 0,
     };
   },
   mounted() {
     // console.log("component mounted");
     setTimeout(() => {
       this.showBlock = true;
+      this.startTimer();
     }, this.delay);
   },
   //   updated() {
@@ -22,6 +25,17 @@ export default {
   //   unmounted() {
   //     console.log("component unmounted");
   //   },
+  methods: {
+    startTimer() {
+      this.timer = setInterval(() => {
+        this.reactionTime += 10;
+      }, 10);
+    },
+    stopTimer() {
+      clearInterval(this.timer);
+      this.$emit("end", this.reactionTime);
+    },
+  },
 };
 </script>
 
