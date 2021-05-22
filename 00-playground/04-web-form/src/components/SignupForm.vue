be26c7cd4bbd299d23a887d19be42a927c432f00
sekai0330 <asamieto965@gmail.com>
2021-05-22 21:11:04 +0200

    add form submit

--- 00-playground/04-web-form/src/components/SignupForm.vue
+++ 00-playground/04-web-form/src/components/SignupForm.vue
@@ -1,9 +1,10 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
     <label>Role:</label>
     <select v-model="role">
       <option value="developer">Web Developer</option>
@@ -18,6 +19,9 @@
       <input type="checkbox" v-model="terms" required />
       <label>Accept terms and conditions</label>
     </div>
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
     <!-- <div>
       <input type="checkbox" value="Shaun" v-model="names" />
       <label>Shaun</label>
@@ -31,10 +35,10 @@
       <label>Mario</label>
     </div> -->
   </form>
-  <p v-if="email">Email: {{ email }}</p>
+  <!-- <p v-if="email">Email: {{ email }}</p>
   <p v-if="password">Password: {{ password }}</p>
   <p v-if="role">Role: {{ role }}</p>
-  <p v-if="terms">Terms: {{ terms }}</p>
+  <p v-if="terms">Terms: {{ terms }}</p> -->
   <!-- <p v-if="names">Names: {{ names }}</p> -->
 </template>
 
@@ -48,6 +52,7 @@ export default {
       tempSkill: "",
       skills: [],
       terms: false,
+      passwordError: "",
       // names: [],
     };
   },
@@ -65,6 +70,19 @@ export default {
         (skill) => skill !== deletedSkill
       );
       this.skills = updatedSkills;
+    },
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+      if (!this.passwordError) {
+        console.log("email: ", this.email);
+        console.log("password: ", this.password);
+        console.log("role: ", this.role);
+        console.log("skills: ", this.skills);
+        console.log("terms accepted: ", this.terms);
+      }
     },
   },
 };
@@ -105,6 +123,12 @@ input[type="checkbox"] {
   position: relative;
   top: 2px;
 }
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
 .pill {
   display: inline-block;
   margin: 20px 10px 0 0;
@@ -116,5 +140,17 @@ input[type="checkbox"] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background-color: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: #fff;
+  border-radius: 20px;
+  cursor: pointer;
+}
+.submit {
+  text-align: center;
 }
 </style>
