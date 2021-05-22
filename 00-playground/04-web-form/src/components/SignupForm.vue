cb6e1abf57134f672df306cce4c31ba990b77ea9
sekai0330 <asamieto965@gmail.com>
2021-05-22 12:47:31 +0200

    add skills

--- 00-playground/04-web-form/src/components/SignupForm.vue
+++ 00-playground/04-web-form/src/components/SignupForm.vue
@@ -9,10 +9,33 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
+    <!-- <div>
+      <input type="checkbox" value="Shaun" v-model="names" />
+      <label>Shaun</label>
+    </div>
+    <div>
+      <input type="checkbox" value="Yoshi" v-model="names" />
+      <label>Yoshi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="Mario" v-model="names" />
+      <label>Mario</label>
+    </div> -->
   </form>
   <p v-if="email">Email: {{ email }}</p>
   <p v-if="password">Password: {{ password }}</p>
   <p v-if="role">Role: {{ role }}</p>
+  <p v-if="terms">Terms: {{ terms }}</p>
+  <!-- <p v-if="names">Names: {{ names }}</p> -->
 </template>
 
 <script>
@@ -22,7 +45,27 @@ export default {
       email: "",
       password: "",
       role: "developer",
+      tempSkill: "",
+      skills: [],
+      terms: false,
+      // names: [],
     };
+  },
+  methods: {
+    addSkill(event) {
+      if (event.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill.slice(0, -1));
+        }
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(deletedSkill) {
+      const updatedSkills = this.skills.filter(
+        (skill) => skill !== deletedSkill
+      );
+      this.skills = updatedSkills;
+    },
   },
 };
 </script>
@@ -54,5 +97,24 @@ select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background-color: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
 }
 </style>
