07991d3ee538aff6dcd16fc921caac928843cfb4
sekai0330 <asamieto965@gmail.com>
2021-05-25 11:22:47 +0200

    fix lifecycle hooks

---
+++ 00-playground/06-dojo-blog/src/components/PostList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted, onUnmounted, onUpdated } from "vue";
+import SinglePost from "./SinglePost";
+
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    // console.log(props.posts);
+    onMounted(() => console.log("component mounted"));
+    onUnmounted(() => console.log("component unmounted"));
+    onUpdated(() => console.log("component updated"));
+  },
+};
+</script>
+
+<style></style>
