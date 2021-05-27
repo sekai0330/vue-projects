1b5fa548a52903fa1c1355cb103aa5248e31c2bc
sekai0330 <asamieto965@gmail.com>
2021-05-27 22:32:25 +0200

    add tagcloud styling

--- 00-playground/06-dojo-blog/src/components/TagCloud.vue
+++ 00-playground/06-dojo-blog/src/components/TagCloud.vue
@@ -21,4 +21,25 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.tag-cloud {
+  padding: 10px;
+}
+.tag-cloud h3 {
+  border-bottom: 1px solid #eee;
+  padding: 16px 8px;
+  color: #444;
+}
+.tag-cloud div {
+  display: inline-block;
+  padding: 10px;
+}
+.tag-cloud a {
+  color: #ccc;
+  text-decoration: none;
+}
+.tag-cloud a.router-link-active {
+  color: #ff8800;
+  font-weight: bold;
+}
+</style>
--- 00-playground/06-dojo-blog/src/views/Tag.vue
+++ 00-playground/06-dojo-blog/src/views/Tag.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="tag">
     <div v-if="error">{{ error }}</div>
-    <div v-if="posts.length">
+    <div v-if="posts.length" class="layout">
       <PostList :posts="taggedPosts" />
+      <TagCloud :posts="posts" />
     </div>
     <div v-else><Spinner /></div>
   </div>
@@ -14,10 +15,11 @@ import { useRoute } from "vue-router";
 
 import PostList from "../components/PostList";
 import Spinner from "../components/Spinner";
+import TagCloud from "../components/TagCloud";
 import getPosts from "../composables/getPosts";
 
 export default {
-  components: { Spinner, PostList },
+  components: { Spinner, PostList, TagCloud },
   setup() {
     const route = useRoute();
     const { posts, error, load } = getPosts();
@@ -33,4 +35,10 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.tag {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 10px;
+}
+</style>
