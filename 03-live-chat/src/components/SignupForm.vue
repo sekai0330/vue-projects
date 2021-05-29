33f770e0a568a2914ec5bdb4f47cd941278dbe50
sekai0330 <asamieto965@gmail.com>
2021-05-29 15:10:46 +0200

    add signup and login

--- 03-live-chat/src/components/SignupForm.vue
+++ 03-live-chat/src/components/SignupForm.vue
@@ -8,24 +8,28 @@
     />
     <input type="email" required placeholder="email" v-model="email" />
     <input type="password" required placeholder="password" v-model="password" />
+    <div class="error">{{ error }}</div>
     <button>Sign Up</button>
   </form>
 </template>
 
 <script>
 import { ref } from "vue";
+import useSignup from "../composables/useSignup";
 
 export default {
   setup() {
+    const { error, signup } = useSignup();
     const displayName = ref("");
     const email = ref("");
     const password = ref("");
 
-    const handleSubmit = () => {
-      console.log(displayName.value, email.value, password.value);
+    const handleSubmit = async () => {
+      await signup(email.value, password.value, displayName.value);
+      console.log("user signed up");
     };
 
-    return { displayName, email, password, handleSubmit };
+    return { displayName, email, password, handleSubmit, error };
   },
 };
 </script>
