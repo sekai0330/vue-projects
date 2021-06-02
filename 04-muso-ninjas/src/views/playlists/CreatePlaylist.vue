5b54d99b4a8ebf82075f442a2839c6c02320f77b
sekai0330 <asamieto965@gmail.com>
2021-06-02 07:39:43 +0200

    add create playlist form

---
+++ 04-muso-ninjas/src/views/playlists/CreatePlaylist.vue
@@ -0,0 +1,47 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <h4>Create New Playlist</h4>
+    <input type="text" placeholder="Playlist title" v-model="title" required />
+    <textarea
+      placeholder="Playlist description..."
+      v-model="description"
+      required
+    ></textarea>
+    <label for="upload">Upload playlist cover image</label>
+    <input type="file" id="upload" />
+    <div class="error"></div>
+    <button>Create</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const title = ref("");
+    const description = ref("");
+
+    const handleSubmit = () => {
+      console.log(title.value, description.value);
+    };
+
+    return { title, description, handleSubmit };
+  },
+};
+</script>
+
+<style scoped>
+input[type="file"] {
+  border: 0;
+  padding: 0;
+}
+label {
+  font-size: 12px;
+  display: block;
+  margin-top: 30px;
+}
+button {
+  margin-top: 20px;
+}
+</style>
