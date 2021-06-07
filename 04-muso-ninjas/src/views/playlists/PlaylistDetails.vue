eef172e2b907116e871341f120770ea08b68d702
sekai0330 <asamieto965@gmail.com>
2021-06-07 07:54:07 +0200

    add displaying songs

--- 04-muso-ninjas/src/views/playlists/PlaylistDetails.vue
+++ 04-muso-ninjas/src/views/playlists/PlaylistDetails.vue
@@ -11,7 +11,16 @@
       <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
     </div>
     <div class="song-list">
-      <p>Song List</p>
+      <div v-if="!playlist.songs.length">
+        No songs have been added to this playlist yet.
+      </div>
+      <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+        <div class="details">
+          <h3>{{ song.title }}</h3>
+          <p>{{ song.artist }}</p>
+        </div>
+        <button v-if="ownership">Delete</button>
+      </div>
       <AddSong v-if="ownership" :playlist="playlist" />
     </div>
   </div>
@@ -97,5 +106,13 @@ export default {
 }
 .description {
   text-align: left;
+}
+.single-song {
+  padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 1px dashed var(--secondary);
+  margin-bottom: 20px;
 }
 </style>
