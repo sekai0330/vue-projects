a61fe2f170723bd8c7192e37ced2c6aed96655ae
sekai0330 <asamieto965@gmail.com>
2021-06-14 07:54:16 +0200

    add transition JS hooks

--- 00-playground/06-animated-todolist/src/components/Todos.vue
+++ 00-playground/06-animated-todolist/src/components/Todos.vue
@@ -6,14 +6,16 @@
       @keypress.enter="addTodo"
       placeholder="Add a new todo..."
     />
-    <div v-if="todos.length">
-      <transition-group tag="ul" name="list" appear>
-        <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
-          {{ todo.text }}
-        </li>
-      </transition-group>
-    </div>
-    <div v-else>Woohoo, nothing left todo!</div>
+    <transition name="switch" mode="out-in">
+      <div v-if="todos.length">
+        <transition-group tag="ul" name="list" appear>
+          <li v-for="todo in todos" :key="todo.id" @click="deleteTodo(todo.id)">
+            {{ todo.text }}
+          </li>
+        </transition-group>
+      </div>
+      <div v-else>Woohoo, nothing left todo!</div>
+    </transition>
   </div>
 </template>
 
@@ -104,7 +106,24 @@ input {
   position: absolute;
 }
 
+/* transition group */
 .list-move {
   transition: all 0.3s ease;
+}
+
+/* switch transitions */
+.switch-enter-from,
+.switch-leave-to {
+  opacity: 0;
+  transform: translateY(20px);
+}
+/* .switch-enter-to,
+.switch-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+} */
+.switch-enter-active,
+.switch-leave-active {
+  transition: all 0.5s ease;
 }
 </style>
--- 00-playground/06-animated-todolist/src/views/About.vue
+++ 00-playground/06-animated-todolist/src/views/About.vue
@@ -1,24 +1,96 @@
 <template>
   <div class="about">
-    <h1>About</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
+    <transition
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      appear
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
+      officia possimus delectus inventore quod quisquam culpa voluptas iusto,
+      quae maiores quo dolorum, corporis laboriosam a dolore consequatur
+      assumenda nam!
+    </p>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
 
 export default {
   setup() {
+    const showTitle = ref(true);
 
-  }
-}
+    const beforeEnter = (el) => {
+      console.log("before enter ", el);
+    };
+    const enter = (el) => {
+      console.log("enter ", el);
+    };
+    const afterEnter = (el) => {
+      console.log("after enter ", el);
+      el.style.color = "green";
+      setTimeout(() => (showTitle.value = false), 2000);
+    };
+    const beforeLeave = (el) => {
+      console.log("before leave ", el);
+      el.style.color = "pink";
+    };
+    const leave = (el) => {
+      console.log("leave ", el);
+    };
+    const afterLeave = (el) => {
+      console.log("after leave ", el);
+    };
+
+    return {
+      beforeEnter,
+      enter,
+      afterEnter,
+      beforeLeave,
+      leave,
+      afterLeave,
+      showTitle,
+    };
+  },
+};
 </script>
 
 <style>
-  .about {
-    max-width: 600px;
-    margin: 20px auto;
-  }
+.about {
+  max-width: 600px;
+  margin: 20px auto;
+}
+
+.fade-enter-from {
+  opacity: 0;
+}
+.fade-enter-active {
+  transition: opacity 3s ease;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: opacity 3s ease;
+}
 </style>
