f1391723dbef6877f675d361b6eba7bc59db2197
sekai0330 <asamieto965@gmail.com>
2021-06-14 08:04:58 +0200

    add GSAP

--- 00-playground/06-animated-todolist/src/views/About.vue
+++ 00-playground/06-animated-todolist/src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <transition
+    <!-- <transition
       name="fade"
       @before-enter="beforeEnter"
       @enter="enter"
@@ -9,8 +9,15 @@
       @leave="leave"
       @after-leave="afterLeave"
       appear
+    > -->
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      appear
     >
-      <h1 v-if="showTitle">About</h1>
+      <!-- <h1 v-if="showTitle">About</h1> -->
+      <h1>About</h1>
     </transition>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam
@@ -35,41 +42,54 @@
 
 <script>
 import { ref } from "vue";
+import gsap from "gsap";
 
 export default {
   setup() {
     const showTitle = ref(true);
 
     const beforeEnter = (el) => {
-      console.log("before enter ", el);
+      console.log("before enter - set initial state", el);
+      el.style.transform = "translateY(-60px)";
+      el.style.opacity = 0;
     };
-    const enter = (el) => {
-      console.log("enter ", el);
+    const enter = (el, done) => {
+      console.log("enter - make transition", el);
+      gsap.to(el, {
+        duration: 1,
+        y: 0,
+        opacity: 1,
+        ease: "bounce.out",
+        onComplete: done,
+      });
     };
     const afterEnter = (el) => {
-      console.log("after enter ", el);
-      el.style.color = "green";
-      setTimeout(() => (showTitle.value = false), 2000);
+      console.log("after enter once done", el);
     };
-    const beforeLeave = (el) => {
-      console.log("before leave ", el);
-      el.style.color = "pink";
-    };
-    const leave = (el) => {
-      console.log("leave ", el);
-    };
-    const afterLeave = (el) => {
-      console.log("after leave ", el);
-    };
+    // const afterEnter = (el) => {
+    //   console.log("after enter ", el);
+    //   el.style.color = "green";
+    //   setTimeout(() => (showTitle.value = false), 2000);
+    // };
+    // const beforeLeave = (el) => {
+    //   console.log("before leave ", el);
+    //   el.style.color = "pink";
+    // };
+    // const leave = (el) => {
+    //   console.log("leave ", el);
+    // };
+    // const afterLeave = (el) => {
+    //   console.log("after leave ", el);
+    // };
 
     return {
       beforeEnter,
       enter,
       afterEnter,
-      beforeLeave,
-      leave,
-      afterLeave,
-      showTitle,
+      // beforeLeave,
+      // leave,
+      // afterLeave,
+      // showTitle,
     };
   },
 };
@@ -81,7 +101,7 @@ export default {
   margin: 20px auto;
 }
 
-.fade-enter-from {
+/* .fade-enter-from {
   opacity: 0;
 }
 .fade-enter-active {
@@ -92,5 +112,5 @@ export default {
 }
 .fade-leave-active {
   transition: opacity 3s ease;
-}
+} */
 </style>
