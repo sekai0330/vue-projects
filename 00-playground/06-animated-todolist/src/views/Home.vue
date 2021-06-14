7fd9890ecbc2253bfba9afb8a43486443334c958
sekai0330 <asamieto965@gmail.com>
2021-06-14 06:58:05 +0200

    add toast notification

--- 00-playground/06-animated-todolist/src/views/Home.vue
+++ 00-playground/06-animated-todolist/src/views/Home.vue
@@ -1,30 +1,77 @@
 <template>
   <div class="home">
-    <Toast v-if="showToast" />
+    <transition name="toast"><Toast v-if="showToast"/></transition>
     <Todos @badValue="triggerToast" />
+    <!-- <transition name="fade">
+      <div v-if="showP">hello, ninjas</div>
+    </transition> -->
+    <button @click="showP = !showP">Toggle</button>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import Toast from '../components/Toast'
-import Todos from '../components/Todos'
+import { ref } from "vue";
+import Toast from "../components/Toast";
+import Todos from "../components/Todos";
 
 export default {
   components: { Toast, Todos },
   setup() {
-    const showToast = ref(false)
+    const showToast = ref(false);
+    // const showP = ref(false);
 
     const triggerToast = () => {
       showToast.value = true;
-      setTimeout(() => showToast.value = false, 3000)
-    }
+      setTimeout(() => (showToast.value = false), 3000);
+    };
 
-    return { showToast, triggerToast }
-  }
-}
+    return { showToast, triggerToast };
+  },
+};
 </script>
 
 <style>
+/* .fade-enter-from {
+  opacity: 0;
+}
+ not needed if default state on the page
+.fade-enter-to {
+  opacity: 1;
+}
+.fade-enter-active {
+  transition: all 2s ease;
+}
+ not needed if default state on the page
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-leave-active {
+  transition: all 2s ease;
+} */
 
+.toast-enter-from {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-enter-active {
+  transition: all 0.3s ease;
+}
+.toast-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+}
+.toast-leave-to {
+  opacity: 0;
+  transform: translateY(-60px);
+}
+.toast-leave-active {
+  transition: all 0.3s ease;
+}
 </style>
