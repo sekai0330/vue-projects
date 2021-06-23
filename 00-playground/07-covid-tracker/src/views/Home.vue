f0140840ef210a736f641877ed2e69d522f2c0c8
sekai0330 <asamieto965@gmail.com>
2021-06-23 07:37:04 +0200

    add data fetching

--- 00-playground/07-covid-tracker/src/views/Home.vue
+++ 00-playground/07-covid-tracker/src/views/Home.vue
@@ -7,5 +7,16 @@
 <script>
 export default {
   name: "Home",
+  methods: {
+    async fetchCovidData() {
+      const res = await fetch("https://api.covid19api.com/summary");
+      const data = await res.json();
+      return data;
+    },
+  },
+  async created() {
+    const data = await this.fetchCovidData();
+    console.log(data);
+  },
 };
 </script>
