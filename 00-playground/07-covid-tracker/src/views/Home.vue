2dcb68e68ef9ba35f8c1960528220032e7d453c4
sekai0330 <asamieto965@gmail.com>
2021-06-24 08:37:46 +0200

    add vue 3 refacto and dayjs

--- 00-playground/07-covid-tracker/src/views/Home.vue
+++ 00-playground/07-covid-tracker/src/views/Home.vue
@@ -1,22 +1,43 @@
 <template>
-  <div class="home">
-    Hello World
-  </div>
+  <main v-if="!loading">
+    <DataTitle :dataDate="dataDate" :text="title" />
+  </main>
+  <main v-else class="flex flex-col align-center justify-center text-center">
+    <div class="text-gray-500 text-3xl mt-10 mb6">Fetching Data</div>
+    <img :src="require('../assets/hourglass.gif')" alt="" class="w-24 m-auto" />
+  </main>
 </template>
 
 <script>
+import { ref } from "vue";
+import DataTitle from "@/components/DataTitle";
+
 export default {
   name: "Home",
-  methods: {
-    async fetchCovidData() {
+  components: { DataTitle },
+  setup() {
+    const loading = ref(true);
+    const title = ref("Global");
+    const dataDate = ref("");
+    const stats = ref({});
+    const countries = ref([]);
+
+    const fetchCovidData = async () => {
       const res = await fetch("https://api.covid19api.com/summary");
-      const data = await res.json();
-      return data;
-    },
-  },
-  async created() {
-    const data = await this.fetchCovidData();
-    console.log(data);
+      return await res.json();
+    };
+
+    const baseSetup = async () => {
+      const data = await fetchCovidData();
+      dataDate.value = data.Date;
+      stats.value = data.Global;
+      countries.value = data.Countries;
+      loading.value = false;
+    };
+
+    baseSetup();
+
+    return { loading, title, dataDate, stats, countries };
   },
 };
 </script>
