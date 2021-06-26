ae989cc9b734c31282c5237808c554e60b07be01
sekai0330 <asamieto965@gmail.com>
2021-06-26 14:00:17 +0200

    add vetur config and basic typescript

--- 00-playground/08-hyrule-jobs/README.md
+++ 00-playground/08-hyrule-jobs/README.md
@@ -2,4 +2,13 @@
 
 A small job board using Vue 3 with TypeScript.
 
+## Vetur
+
+Make sure to install [Vetur](https://marketplace.visualstudio.com/items?itemName=octref.vetur) in VS Code.
+
+If your template validation is not working, check these settings are both enabled:
+
+1. Vetur-> Validation -> Interpolation
+2. Vetur-> Experimental-> Template Interpolation Service
+
 Based on [Vue 3 with TypeScript Tutorial](https://www.youtube.com/playlist?list=PL4cUxeGkcC9gCtAuEdXTjNVE5bbMFo5OD) by Shaun Pelling - The Net Ninja (2021).
--- 00-playground/08-hyrule-jobs/src/App.vue
+++ 00-playground/08-hyrule-jobs/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="app">hello, hyrule</div>
+  <div class="app">
+    <p>{{ name }} - {{ age }}</p>
+    <button @click="changeName('Zelda')">Change Name</button>
+    <button @click="changeAge(33)">Change Age</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -7,6 +11,23 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   name: "App",
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+      return name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+      return age;
+    },
+  },
 });
 </script>
 
