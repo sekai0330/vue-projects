8f6f8d3033cd4612f30640914983ab8254c364c6
sekai0330 <asamieto965@gmail.com>
2021-06-26 14:57:29 +0200

    add jobList

--- 00-playground/08-hyrule-jobs/src/App.vue
+++ 00-playground/08-hyrule-jobs/src/App.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="app">
-    {{ jobs[0].location }}
+    <JobList :jobs="jobs" />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import Job from "./types/Job";
+import JobList from "./components/JobList.vue";
 
 export default defineComponent({
   name: "App",
-  components: {},
+  components: { JobList },
   setup() {
     const jobs = ref<Job[]>([
       {
---
+++ 00-playground/08-hyrule-jobs/src/components/JobList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="job-list">
+    <ul>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <img src="../assets/rupee.svg" alt="rupee icon" />
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+            Consectetur nemo et praesentium, saepe voluptatum ipsam dignissimos
+            culpa quae. Accusamus sequi veniam velit corporis quis consequatur
+            repudiandae ab officia illum expedita?
+          </p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+import Job from "../types/Job";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+  },
+});
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background-color: #fff;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17b661;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+</style>
