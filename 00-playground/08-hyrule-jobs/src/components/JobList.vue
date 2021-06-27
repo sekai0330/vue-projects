09537f33922577af30f2ae606e1c8ce232b659ba
sekai0330 <asamieto965@gmail.com>
2021-06-27 07:28:00 +0200

    add computed values

--- 00-playground/08-hyrule-jobs/src/components/JobList.vue
+++ 00-playground/08-hyrule-jobs/src/components/JobList.vue
@@ -2,7 +2,7 @@
   <div class="job-list">
     <p>Ordered by {{ order }}</p>
     <ul>
-      <li v-for="job in jobs" :key="job.id">
+      <li v-for="job in orderedJobs" :key="job.id">
         <h2>{{ job.title }} in {{ job.location }}</h2>
         <div class="salary">
           <img src="../assets/rupee.svg" alt="rupee icon" />
@@ -22,7 +22,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "vue";
+import { defineComponent, PropType, computed } from "vue";
 import Job from "../types/Job";
 import OrderTerm from "../types/OrderTerm";
 
@@ -36,6 +36,14 @@ export default defineComponent({
       required: true,
       type: String as PropType<OrderTerm>,
     },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
   },
 });
 </script>
